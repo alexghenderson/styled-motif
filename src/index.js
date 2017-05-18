f66b836671c947e4d22d0140322d933f6397c913
Alex Henderson <public.lieWgmail.com>
2017-05-17 19:46:01 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import Color from 'color';
+
+// converts theme to object of colors
+const recurse = (theme) => {
+  const out = Object.keys(theme).reduce(
+    (palette, key) => {
+      palette[key] = (
+        typeof theme[key] === 'object'
+        ? (palette[key] = recurse(theme[key]))
+        : (palette[key] = Color(theme[key]))
+      );
+      return palette;
+    }
+  , {});
+  return out;
+};
+
+const colors = (selector, theme) => {
+  return selector(recurse(theme));
+};
+
+const path = (key, theme) => {
+  const paths = key.split('.');
+  const out = paths.reduce(
+    (result, index) => {
+      return result[index];
+    }
+  , theme);
+
+  return out;
+};
+
+const motif = (arg) => (props) => {
+  return (
+    typeof arg === 'function'
+    ? colors(arg, props.theme)
+    : path(arg, props.theme)
+  );
+};
+
+export default motif;
